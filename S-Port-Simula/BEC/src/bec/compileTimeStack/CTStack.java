c1587eaf7cb9bdfd3cb420b91afe64522609b9bc
Øystein Myhre Andersen <o.myhre@gmail.com>
2025-10-22 14:31:30 +0200

    upd

--- S-Port-Simula/BEC/src/bec/compileTimeStack/CTStack.java
+++ S-Port-Simula/BEC/src/bec/compileTimeStack/CTStack.java
@@ -103,12 +103,10 @@ public class CTStack {
 	}
 	
 	public static void pushTempVAL(Type type, int count) {
-//		IO.println("CTStack.pushTempVAL: " + type + " " + count + " " + comment+"  STACK="+stack.ident);
 		push(new Temp(CTStackItem.Mode.VAL, type, count));
 	}
 	
 	public static void pushTempREF(Type type, int count) {
-//		IO.println("CTStack.pushTempREF: " + type + " " + count + " " + comment);
 		push(new Temp(CTStackItem.Mode.REF, type, count));
 	}
 	
@@ -117,38 +115,12 @@ public class CTStack {
 	}
 	
 	public static CTStackItem pop() {
-//		if(stack.size() == 1) RTRegister.clearFreeRegs();
 		return stack.pop();
 	}
 	
 	public static void dup() {
-//		IO.println("CTStack.dup: TOS=" + TOS());
 		stack.push(TOS().copy());
 	}
-
-//	public static void main(String[] argv) {
-//		CTStack.push(new ConstItem(Type.T_INT, IntegerValue.of(Type.T_INT, 111) ));
-//		CTStack.push(new ConstItem(Type.T_INT, IntegerValue.of(Type.T_INT, 222) ));
-//		CTStack.push(new ConstItem(Type.T_INT, IntegerValue.of(Type.T_INT, 333) ));
-//		CTStack.push(new ConstItem(Type.T_INT, IntegerValue.of(Type.T_INT, 444) ));
-//		CTStack.dumpStack("TEST-1");
-//		IO.println("TOS = " + CTStack.TOS());
-//		IO.println("SOS = " + CTStack.SOS());
-//		IO.println("Size = " + CTStack.size());
-//		CTStack.pop();
-//		CTStack.dumpStack("TEST-2");
-//		IO.println("TOS = " + CTStack.TOS());
-//		IO.println("SOS = " + CTStack.SOS());
-//		IO.println("Size = " + CTStack.size());
-//		
-//		CTStack.push(new ConstItem(Type.T_INT, IntegerValue.of(Type.T_INT, 555) ));
-//		CTStack.saveState();
-//		CTStack.dumpStack("TEST-3");
-//		CTStack.push(new ConstItem(Type.T_INT, IntegerValue.of(Type.T_INT, 666) ));
-//		CTStack.dumpStack("TEST-4");
-//		CTStack.restoreState();
-//		CTStack.dumpStack("TEST-5");
-//	}
 	
 	private static void STKERR(String msg) {
 		IO.println("\nERROR: " + msg + " ================================================");
@@ -193,7 +165,6 @@ public class CTStack {
 	}
 
 	public static void checkTosArith() {
-//		IO.println("CTStack.checkTosArith: " + xTOS.type);
 		switch(TOS().type.tag) {
 			case Scode.TAG_INT, Scode.TAG_SINT, Scode.TAG_REAL, Scode.TAG_LREAL: break; 
 			default: STKERR("Illegal type of TOS");
@@ -225,12 +196,9 @@ public class CTStack {
 		Type t1 = TOS().type;
 		Type t2 = SOS().type;
 		if(t1 == t2) return;
-//		if(t1 > Scode.TAG_SIZE) STKERR("CODER.CheckTypesEqual-1: " + Scode.edTag(t1));
-//		if(t2 > Scode.TAG_SIZE) STKERR("CODER.CheckTypesEqual-2: " + Scode.edTag(t2));
 		t1 = arithType(t1,t1); t2 = arithType(t2,t2);
 		if(t1 == t2) return;
 		if(arithType(t1,t2).tag == Type.T_INT.tag) return;
-//	%+C       if (t1>T_BYT1) or (t2>T_BYT1)
 		Type.dumpTypes("checkTypesEqual: ");
 		STKERR("Different types of TOS=" + t1 + " and SOS=" + t2);
 	}
